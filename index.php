98f4428b11ce792f60c1615be9a0c9871ae42627
Stefan T <stefan.traut@students.fhv.at>
2018-02-03 17:31:16 +0100

    updated index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 
 <body>
 
-<iframe src="/generated/index.html" style="height: 50%;"></iframe> 
+<iframe src="/generated/index.html" style="height: 50%; width: 100%;"></iframe> 
  
-<iframe src="/genreated/apidocs" style="height: 50%;"></iframe> 
+<iframe src="/genreated/apidocs/index.html" style="height: 50%; width: 100%;"></iframe> 
 
 </body>
 </html>
